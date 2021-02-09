061b56fc9cdcd046954e3e3ebbd4a73e0d6586db
Doug Boari <doug@tastymushroom.com>
2021-02-09 12:02:28 +0000

    Add -Wlogical-op warning and fix errors.

--- cmake_modules/RomiProjectBuildOptions.cmake
+++ cmake_modules/RomiProjectBuildOptions.cmake
@@ -19,7 +19,7 @@ endif()
 # For now everything is debug. Set Compile options locally to maintain independent library builds.
 # Ideal list of warnings! Add gradually!
 #-Werror -Wall -Wpedantic -Wextra -Wmissing-include-dirs -Wuseless-cast -Wconversion -Wsign-conversion -Wswitch-default -Weffc++ -Wswitch-enum -Wzero-as-null-pointer-constant -Winit-self -Wformat=2 -Waddress -Wlogical-op -Wpointer-arith)
-set(COMMON_COMPILATION_FLAGS "-Wall -Wextra -Wpedantic -Werror -Wmissing-include-dirs -Wconversion -Wsign-conversion -Wswitch-default -Wswitch-enum -Winit-self -Waddress")
+set(COMMON_COMPILATION_FLAGS "-Wall -Wextra -Wpedantic -Werror -Wmissing-include-dirs -Wconversion -Wsign-conversion -Wswitch-default -Wswitch-enum -Winit-self -Waddress -Wlogical-op")
 set(CMAKE_BUILD_TYPE Debug)
 set(CMAKE_CXX_FLAGS "${COMMON_COMPILATION_FLAGS} -Wuseless-cast -Weffc++ -Wzero-as-null-pointer-constant ${PROJECT_SANITISE_FLAGS}")
 set(CMAKE_CXX_FLAGS_DEBUG "-O0")
