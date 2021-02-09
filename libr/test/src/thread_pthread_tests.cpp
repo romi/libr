e05b8e4146ad63ce3370b64b924aea97cbffe851
Doug Boari <doug@tastymushroom.com>
2021-02-09 11:48:44 +0000

    Add -Wzero-as-null-pointer-constant warning and fix errors.

--- libr/test/src/thread_pthread_tests.cpp
+++ libr/test/src/thread_pthread_tests.cpp
@@ -205,8 +205,7 @@ TEST_F(thread_pthread_tests, new_mutex_returns_mutex)
 TEST_F(thread_pthread_tests, delete_mutex_deletes_mutex)
 {
     // Arrange
-    pthread_mutex_t pthread_mutex  = PTHREAD_MUTEX_INITIALIZER;
-
+    pthread_mutex_t pthread_mutex;
     mutex_t mutex_data;
     mutex_data.mutex = pthread_mutex;
 
