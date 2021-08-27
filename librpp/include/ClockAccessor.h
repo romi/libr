a1472e6265db646bf5f1365f3c251c9fee4684c7
Doug Boari <doug@tastymushroom.com>
2021-08-27 10:23:20 +0200

    Add scoped lock to clock accessor.

--- librpp/include/ClockAccessor.h
+++ librpp/include/ClockAccessor.h
@@ -2,6 +2,7 @@
 #define ROMI_ROVER_BUILD_AND_TEST_CLOCKACCESSOR_H
 
 #include <memory>
+#include <mutex>
 #include "Clock.h"
 
 namespace rpp {
@@ -11,17 +12,22 @@ namespace rpp {
             public:
                 static const std::shared_ptr<IClock>& GetInstance()
                 {
+                    {
+                        std::scoped_lock lock(clock_mutex);
                         if (g_clock == nullptr)
-                                g_clock = std::make_shared<rpp::Clock>();
-                        return g_clock;
+                            g_clock = std::make_shared<rpp::Clock>();
+                    }
+                    return g_clock;
                 }
 
                 static void SetInstance(const std::shared_ptr<IClock>& globalClock)
                 {
-                      g_clock = globalClock;
+                    std::scoped_lock lock(clock_mutex);
+                    g_clock = globalClock;
                 }
 
             private:
+                static std::mutex clock_mutex;
                 static std::shared_ptr<IClock> g_clock;
     };
 }
--- librpp/src/ClockAccessor.cpp
+++ librpp/src/ClockAccessor.cpp
@@ -2,4 +2,7 @@
 
 namespace rpp{
     std::shared_ptr< IClock > ClockAccessor::g_clock;
+    std::mutex ClockAccessor::clock_mutex;
+
+
 }
