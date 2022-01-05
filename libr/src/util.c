84b383d4f944906e7b02d935d05aaea28c9a7652
Doug Boari <doug@tastymushroom.com>
2022-01-05 15:11:23 +0100

    Remove random and UUID functionality. Random functionality moved to librpp.

--- libr/src/util.c
+++ libr/src/util.c
@@ -24,7 +24,6 @@
  */
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/syscall.h>
 #include "r.h"
 
 char *rprintf(char *buffer, size_t buflen, const char *format, ...)
@@ -49,26 +48,3 @@ char *rprintf(char *buffer, size_t buflen, const char *format, ...)
         return buffer;
 }
 
-int r_random(void *buf, size_t buflen)
-{
-        int flags = 0; 
-        return (int) syscall(SYS_getrandom, buf, buflen, flags);
-}
-
-char *r_uuid()
-{
-        uint8_t b[16];
-        r_random(&b, sizeof(b));
-
-        // RFC 4122 section 4.4
-        b[6] = (uint8_t)(0x40 | (b[6] & 0x0f));
-        b[8] = (uint8_t)(0x80 | (b[8] & 0x3f));
-        
-        char s[37];
-        snprintf(s, 37,
-                 "%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
-                 b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7],
-                 b[8], b[9], b[10], b[11], b[12], b[13], b[14], b[15]);
-
-        return r_strdup(s);
-}
